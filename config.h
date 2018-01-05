c2249152efd3e43f6dbf39120c35c801e6c0d927
Vladislav Ivanishin <vladislav.ivanishin@gmail.com>
2018-01-05 20:09:17 +0300

    st -e tmux

--- config.h
+++ config.h
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st", "-e", "tmux", NULL };
 static const char *volup_cmd[]  = { "amixer", "set", "Master", "1%+", NULL };
 static const char *voldown_cmd[]  = { "amixer", "set", "Master", "1%-", NULL };
 static const char *voltoggle_cmd[]  = { "amixer", "set", "Master", "toggle", NULL };
